371f1e5b5a9e495c620987da93af7101f443949c
Joe Heck <heckj@mac.com>
2015-01-09 12:18:46 -0800

    adding test structure, jslintrc

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ var di = require('di'),
     injector = new di.Injector(
         _.flatten([
             core.injectables,
-            require('./app'),
-            require('./server')
+            require('./lib/app'),
+            require('./lib/server')
         ])
     ),
     logger = injector.get('Logger').initialize('Tftp'),
     tftp = injector.get('Tftp');
 
 tftp.start()
-  .catch(function(err) {
+    .catch(function (err) {
         logger.error('Failure starting TFTP service', {
             error: err
         });
-
-        process.nextTick(function(){
-            process.exit(1);
-        });
-  });
-
-process.on('SIGINT',function() {
-  tftp.stop()
-    .catch(function(err) {
-        logger.error('Failure cleaning up TFTP messenger', {
-            error: err
-        });
-    })
-    .fin(function() {
-        process.nextTick(function(){
+        process.nextTick(function () {
             process.exit(1);
         });
     });
+
+process.on('SIGINT', function () {
+    tftp.stop()
+        .catch(function (err) {
+            logger.error('Failure cleaning up TFTP messenger', {
+                error: err
+            });
+        })
+        .fin(function () {
+            process.nextTick(function () {
+                process.exit(1);
+            });
+        });
 });
+
