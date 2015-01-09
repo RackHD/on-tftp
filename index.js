9c0ae7afe20a1a2b4a6f4c5c10c3e8e4c57c82ed
Ben Broderick Phillips <ben@benbp.net>
2015-01-09 14:22:36 -0800

    Update error prints to use console log

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 // Copyright 2014, Renasar Technologies Inc.
 /* jshint node: true */
-
-'use strict';
+"use strict";
 
 var di = require('di'),
     _ = require('lodash'),
     core = require('renasar-core')(di),
     injector = new di.Injector(
-        _.flatten([
-            core.injectables,
-            require('./lib/app'),
-            require('./lib/server')
-        ])
+      _.flatten([
+        core.injectables,
+        require('./lib/app'),
+        require('./lib/server')
+      ])
     ),
-    logger = injector.get('Logger').initialize('Tftp'),
     tftp = injector.get('Tftp');
 
 tftp.start()
-    .catch(function (err) {
-        logger.error('Failure starting TFTP service', {
-            error: err
-        });
-        process.nextTick(function () {
-            process.exit(1);
-        });
-    });
+.catch(function(err) {
+  console.error('Failure starting TFTP service' + err.stack);
+  process.nextTick(function(){
+    process.exit(1);
+  });
+});
 
-process.on('SIGINT', function () {
-    tftp.stop()
-        .catch(function (err) {
-            logger.error('Failure cleaning up TFTP messenger', {
-                error: err
-            });
-        })
-        .fin(function () {
-            process.nextTick(function () {
-                process.exit(1);
-            });
-        });
+process.on('SIGINT',function() {
+  tftp.stop()
+  .catch(function(err) {
+    console.error('Failure cleaning up TFTP service' + err.stack);
+  })
+  .fin(function() {
+    process.nextTick(function(){
+      process.exit(1);
+    });
+  });
 });
 
