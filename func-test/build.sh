e369e4feea8263afc02765d3d097b5cb8dd6de6f
andre keedy <andre.keedy@emc.com>
2017-06-12 17:01:28 -0400

    not needed to build dockers

--- func-test/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "hello world"
-docker daemon  
-
-cd  $(pwd)/source-code 
-docker build -t rackhd/on-tftp .
-docker tag rackhd/on-tftp 10.240.16.225:5000
-docker push 10.240.16.225:5000:rckhd/on-tftp
