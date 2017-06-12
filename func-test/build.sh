774cf38158331e243865348d77b5b01fa212d309
andre keedy <andre.keedy@emc.com>
2017-06-12 14:20:33 -0400

    debug

--- func-test/build.sh
+++ func-test/build.sh
@@ -1,4 +1,9 @@
-cd $pwd/../ 
+#!/bin/sh
+
+echo "hello world"
+docker daemon  
+
+cd  $(pwd)/source-code 
 docker build -t rackhd/on-tftp .
 docker tag rackhd/on-tftp 10.240.16.225:5000
 docker push 10.240.16.225:5000:rckhd/on-tftp
